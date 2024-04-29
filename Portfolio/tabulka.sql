27ac4db019acebc9b8911189556334a066140419
Jan Maláč <165727116+JanMalac@users.noreply.github.com>
2024-04-29 07:49:38 +0200

    Update tabulka.sql

--- Portfolio/tabulka.sql
+++ Portfolio/tabulka.sql
@@ -1,13 +1,13 @@
 SELECT predmet AS předmět,
-        SUM(*) AS počet,
+        COUNT(*) AS kolik,
         SUM(bod) AS suma,
         ROUND(SUM(bod) / COUNT(*), 2) AS prumer,
         CASE
             WHEN SUM(bod) < 10 THEN 'Je v řiti.'
-            WHEN SUM(bod) > 10 && SUM(bod) < 15 THEN 'Ještě by se měl hodně snažit.'
-            WHEN SUM(bod) > 15 && SUM(bod) < 20 THEN 'Ještě by se měl trochu snažit.'
-            WHEN SUM(bod) > 20 && SUM(bod) < 30 THEN 'Pohodička.'
+            WHEN SUM(bod) >= 10 && SUM(bod) < 15 THEN 'Ještě by se měl hodně snažit.'
+            WHEN SUM(bod) >= 15 && SUM(bod) < 20 THEN 'Ještě by se měl trochu snažit.'
+            WHEN SUM(bod) >= 20 && SUM(bod) < 30 THEN 'Pohodička.'
             WHEN SUM(bod) >= 30 THEN 'Naprostá pohodička.'
-        END AS 'jak jsi na tom'
+            END AS 'stav'
 FROM vysledky
 GROUP BY predmet;
